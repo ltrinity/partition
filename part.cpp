c2a9f60fabfea4040f671760f6121176f5cd1cdc
Luke Trinity <Ltrinity@uvm.edu>
2023-06-27 01:20:50 -0700

    updated git ignore, basic hairpin following sparse approach

---
+++ part.cpp
@@ -0,0 +1,260 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <iomanip>
+#include <cstring>
+#include <string>
+
+bool DEBUG = false;
+
+// constraints
+int minLoop = 3;
+
+// penalties
+float unpairedCost = 0.1;
+float unpairedMLCost = 0.1;
+float bpMLCost = 0.5;
+float MLinitCost = 1;
+
+//constants
+float temp = 37.0;
+float gasConstant = 0.0821;
+
+// index vector saves space
+// we only need the upper triangle
+std::vector<int> indexVec;
+
+// calculate matrix index vis a vis vector
+int matInd(int i, int j){
+    return (indexVec[i] + j - i);
+}
+
+// boltzmann function
+float Bolt(float energy){
+    return exp(-energy/(temp*gasConstant));
+}
+
+// canonical pair checker
+std::pair<bool, double> canPair(char base1, char base2) {
+    // two or three hydrogen bonds for AU or CG respectively
+    if ((base1 == 'A' && base2 == 'U') || (base1 == 'U' && base2 == 'A')){
+        return std::make_pair(true, -2.0);
+    } else if ((base1 == 'G' && base2 == 'C') || (base1 == 'C' && base2 == 'G')) {
+        return std::make_pair(true, -3.0);
+    } else {
+        return std::make_pair(false, 0.0);
+    }
+}
+
+// vectir printer function
+void printVector(const double* M, std::string sequence){
+    // Print the sequence
+    std::cout << "\t";
+    for (int i = 0; i < sequence.length(); i++) {
+        std::cout << sequence[i]<< "\t" ;
+    }
+    std::cout << "\n";
+
+    int size = sequence.length();
+    // Print the matrix
+    for (int i = 0; i < size; i++) {
+        std::cout << sequence[i] << "\t";
+        //empty spaces for bottom triangle
+        int x = i;
+        while( x > 0){
+            std::cout << "\t";
+            x--;
+        }
+        for (int j = 0; j < size-i; j++) {
+            std::cout << std::setprecision(2) << M[indexVec[i] + j] << "\t";
+        }
+        std::cout << std::endl;
+    }
+}
+
+// Compute the pf using simple energy model
+void computePartitionFunction(std::string sequence, int n, int minLoop) {
+    //fill index vector
+    int vectorLength = (n*(n+1))/2;
+    int rowInc = n;
+    for(int i = 1; i <= n; i++){
+        indexVec[i] += indexVec[i-1]+rowInc;
+        rowInc--;
+    }
+    
+    // Initialize V vector
+    double* V = new double[vectorLength];
+    // Initialize WM1 vector
+    double* WM1 = new double[vectorLength];
+    // Initialize WM vector
+    double* WM = new double[vectorLength];
+    // Initialize VM vector
+    double* VM = new double[vectorLength];
+
+    int count = 0;
+
+    // Compute V matrix
+    for (int i = n-1-minLoop; i >= 0; i--) {
+        for (int j = n-1; j >= i + minLoop; j--) {
+            auto hairpin = canPair(sequence[i], sequence[j]);
+            bool hairpin_possible = hairpin.first;
+            int hairpin_energy = hairpin.second;
+            if (hairpin_possible) {
+                // hairpin
+                count += 1;
+                int ijLoc = matInd(i,j);
+                int hairpinLoopSize = j-i-1;
+	            V[ijLoc] = Bolt(unpairedCost*hairpinLoopSize + hairpin_energy);
+                // stacked pair
+                auto stackedPair = canPair(sequence[i+1], sequence[j-1]);
+                bool stack_possible = stackedPair.first;
+                int stackedPairEnergy = stackedPair.second;
+                if(stack_possible){
+                    count += 1;
+                    float stackSumEnergy = V[matInd(i+1,j-1)] * Bolt(stackedPairEnergy);
+                    V[ijLoc] += stackSumEnergy;
+                }
+                // internal bulge
+                for(int m = j-2; m > 0; m--){
+                    int k = i + 1;
+                    int l = m;
+                    // possibly remove this check
+                    while (l <= j-1 && k < l-minLoop){
+                        auto internalPair = canPair(sequence[k], sequence[l]);
+                        bool internal_possible = internalPair.first;
+                        int internalEnergy = internalPair.second;
+                        if(internal_possible){
+                            count += 1;
+                            // unpaired count in between the pairs
+                            int internalLoopSize = (k-i-1) + (j-l-1);
+                            float internalSumEnergy = V[matInd(k,l)] * Bolt(internalEnergy + internalLoopSize*unpairedCost);
+                            V[ijLoc] += internalSumEnergy;
+                        }
+                        k++;
+                        l++;
+                    }
+                }
+                // multiloop
+                // terminal (rightmost) branch
+                WM1[ijLoc] += (V[ijLoc] * Bolt(bpMLCost));
+                
+                // option for j unpaired and penalty inside of multiloop
+                int ijsub1Loc = matInd(i, j-1);
+
+                // move to terminal branch i,j-1 (j unpaired)
+                WM1[ijLoc] += (WM1[ijsub1Loc] * Bolt(unpairedMLCost));
+
+                // unpaired base between branches (j unpaired)
+                WM[ijLoc] += (WM[ijsub1Loc] * Bolt(unpairedMLCost));
+
+                // option for additional branch 
+                for (int r = i; r < j-1; r++){
+                    //initial branch
+                    WM[ijLoc] += (V[matInd(r,j)] * Bolt(unpairedMLCost*(r-i) + bpMLCost));
+
+                    // intermediate branch
+                    WM[ijLoc] += (WM[matInd(i,r)] * V[matInd(r+1,j)] * Bolt(bpMLCost));
+                }
+
+                // at least two branches required for multiloop
+                for (int h = i+2; h <= j-1; h++){
+                        VM[ijLoc] += (WM[matInd(i+1,h-1)] * WM1[matInd(h,j-1)] * Bolt(bpMLCost + MLinitCost));
+                        V[ijLoc] += (VM[ijLoc]);
+                        if(VM[ijLoc] > 0){
+                            count += 1;
+                        }
+                }
+            }
+        }
+    }
+
+    // Initialize W vector
+    double* W = new double[sequence.length()];
+    // base case
+    W[0] = 1.0;
+    // Compute W
+    for (int j = 1; j < n; j++) {
+        for (int r = 0; r < j; r++){
+            W[j] += (std::max(W[r-1],1.0)*V[matInd(r,j)]);
+        }
+        W[j] += W[j-1];
+    }
+    
+    if(DEBUG){
+        std::cout << "V" << std::endl;
+        printVector(V, sequence);
+        std::cout << "\nWM1" << std::endl;
+        printVector(WM1, sequence);
+        std::cout << "\nWM" << std::endl;
+        printVector(WM, sequence);
+        std::cout << "\nVM" << std::endl;
+        printVector(VM, sequence);
+        std::cout << "\nW" <<std::endl;
+         // Print W
+        std::cout << "\t";
+        for (int i = 0; i < sequence.length(); i++) {
+            std::cout << sequence[i]<< "\t" ;
+        }
+        std::cout << "\n\t";
+        for (int i = 0; i < sequence.length(); i++) {
+            std::cout << W[i] << "\t" ;
+        }
+        std::cout << "\n";
+    }
+    
+    std::cout << "count: " << count << std::endl;
+
+    std::cout << "partition function: " << W[indexVec[0]+(n-1)] << std::endl;
+
+    // PIPE DOTPLOT VIA Gnuplot
+    // Open Gnuplot
+    FILE* gnuplotPipe = popen("gnuplot -persist", "w");
+    //output png
+    fprintf(gnuplotPipe, "set terminal png; set output 'plot.png'\n");
+    //set acis
+    fprintf(gnuplotPipe, "set yrange [0:%d]; set xrange [0:%d]\n",n,n);
+    //set ticks
+    fprintf(gnuplotPipe, "set tics scale 0;set xtics format ''\n");
+    fprintf(gnuplotPipe, "set x2tics(");
+    for(float i = 0;i< n-1;i++){
+        fprintf(gnuplotPipe, "'%c' %f, ",sequence.at(i),i+0.5) ;   
+    }
+    fprintf(gnuplotPipe, "'%c' %f)\n",sequence.at(n-1),n-0.5) ;  
+    fprintf(gnuplotPipe, "set ytics(");
+    for(float i = 0;i< n-1;i++){
+        fprintf(gnuplotPipe, "'%c' %f, ",sequence.at(n-i-1),i+0.5) ;   
+    }
+    fprintf(gnuplotPipe, "'%c' %f)\n",sequence.at(0),n-0.5) ;  
+    fprintf(gnuplotPipe, "set grid ytics x2tics\n"); 
+    //init rect from V/W[0,n-1] with sqrt scaling
+    int idx = 0;
+    for(int i=n-1; i >= 0; i--) {
+      for(int j=n-i-1; j < n; j++) {
+          double size = sqrt(V[idx++]/W[indexVec[0]+(n-1)]);
+          fprintf(gnuplotPipe, "set object %d rect center %f, %f size %f, %f fc black\n", idx, j+0.5, i+0.5, size, size);
+
+      }
+    }
+    fprintf(gnuplotPipe, "plot 0 notitle\n");
+
+    // Close the pipe
+    pclose(gnuplotPipe);
+}
+
+int main() {
+    std::cout << std::fixed;
+    std::cout << std::setprecision(2);
+    // basic hairpin
+    //std::string seq = "CAAAG";
+    // basic stack
+    //std::string seq = "CCAAAGG";
+    // basic internal
+    //std::string seq = "CACAAAGG";
+    // basic multiloop
+    std::string seq = "CCAAAGCAAAGG";
+    int n = seq.length();
+    indexVec.resize(n, 0);
+    std::cout << seq << std::endl;
+    computePartitionFunction(seq, n, minLoop);
+    return 0;
+}
---
+++ src/CParty.cpp
@@ -0,0 +1,273 @@
+/**
+* @mainpage
+*
+* Conditional partition function for density-2 RNA pseudoknots
+*/
+#include "base_types.hh"
+#include "cmdline.hh"
+#include "matrix.hh"
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <iterator>
+#include <cstring>
+#include <string>
+#include <cassert>
+extern "C" {
+#include "ViennaRNA/pair_mat.h"
+#include "ViennaRNA/loops/all.h"
+#include "ViennaRNA/params/io.h"
+}
+
+class CParty;
+
+energy_t ILoopE(auto const& S_,auto const& S1_, auto const& params_,const int& ptype_closing, const cand_pos_t i, const cand_pos_t j, const cand_pos_t k,  const cand_pos_t l);
+
+bool evaluate_restriction(int i, int j, int32_t *last_j_array, int32_t *in_pair_array);
+
+/**
+* Methods for the computation of the partition function
+* and associated structure classes.
+*/
+class CParty {
+	public:
+		std::string seq_;
+		cand_pos_t n_;
+
+		short *S_;
+		short *S1_;
+
+		paramT *params_;
+		std::string restricted_;
+
+        bool garbage_collect_;
+
+		LocARNA::Matrix<energy_t> V_; // store V[i..i+MAXLOOP-1][1..n]
+
+		CParty(const std::string &seq, bool garbage_collect, std::string restricted)
+		: seq_(seq),n_(seq.length()),params_(scale_parameters()),garbage_collect_(garbage_collect){
+
+        make_pair_matrix();
+
+        S_ = encode_sequence(seq.c_str(),0);
+        S1_ = encode_sequence(seq.c_str(),1);
+		V_.resize(MAXLOOP+1,n_+1,INF);
+		restricted_ = restricted;
+        }
+
+        
+
+        ~CParty() {
+        free(params_);
+        free(S_);
+        free(S1_);
+        }
+    };
+
+	/**
+	 * @brief This code returns the hairpin energy for a given base pair.
+	 * @param i The left index in the base pair
+	 * @param j The right index in the base pair
+	*/
+	energy_t HairpinE(auto const& seq, auto const& S, auto const& S1, auto const& params, cand_pos_t i, cand_pos_t j) {
+		
+		const pair_type ptype_closing = pair[S[i]][S[j]];
+
+		if (ptype_closing==0) return INF;
+
+		return E_Hairpin(j-i-1,ptype_closing,S1[i+1],S1[j-1],&seq.c_str()[i-1], const_cast<paramT *>(params));
+	}
+
+	/**
+	 * @brief Evaluates whether a pairing can occur based on the restriction
+	 * 
+	 * @param i Current i
+	 * @param j Current j
+	 * @param p_table Restricted array
+	 * @param last_j_array Restricted array
+	 * @param in_pair_array Restricted array
+	 * @return whether i and j can be non INF 
+	 */
+	bool evaluate_restriction(cand_pos_t i, cand_pos_t j, const cand_pos_t *last_j_array, const cand_pos_t *in_pair_array){
+		bool evaluate = 1;
+		// if(in_pair_array[i]>in_pair_array[j]) evaluate = 0;
+		evaluate &= ~(in_pair_array[i]>in_pair_array[j]);
+		// if(in_pair_array[i]<in_pair_array[j]) evaluate = 0;
+		evaluate &= ~(in_pair_array[i]<in_pair_array[j]);
+		// if(in_pair_array[i]==in_pair_array[j]) if(j>last_j_array[i]) evaluate = 0
+		evaluate &= ((in_pair_array[i]==in_pair_array[j]) & ~(j>last_j_array[i]));
+		
+		return evaluate;
+	}
+	/**
+ 	* @brief Determines the MFE energy for a given sequence
+	*/
+	energy_t fold(auto const& seq, auto &V, auto const& S, auto const& S1, auto const& params, auto const& n, auto const& garbage_collect,const cand_pos_t*p_table,const cand_pos_t*last_j_array,const cand_pos_t*in_pair_array,const cand_pos_t*up_array) {
+		for (cand_pos_t i=n; i>0; --i) {
+		 	for (cand_pos_t j=i+TURN+1; j<=n; j++ ) {
+				std::cout << i << j << std::endl;
+				std::cout << "evaluate" << std::endl;
+		 		bool evaluate = evaluate_restriction(i,j,last_j_array,in_pair_array);
+				cand_pos_t i_mod= (uint_least32_t)i%(MAXLOOP+1);
+		 		const pair_type ptype_closing = pair[S[i]][S[j]];
+		 		const bool restricted = p_table[i] == -1 || p_table[j] == -1;
+
+		 		const bool unpaired = (p_table[i]<-1 && p_table[j]<-1);
+		 		const bool paired = (p_table[i] == j && p_table[j] == i);
+		 		energy_t v = INF;
+		 		// ----------------------------------------
+		 		// cases with base pair (i,j)
+		 		if(ptype_closing>0 && evaluate && !restricted) { // if i,j form a canonical base pair
+		 			bool canH = (paired || unpaired);
+		 			if(up_array[j-1]<(j-i-1)) canH=false;
+					
+		 			energy_t v_h = canH ? HairpinE(seq,S,S1,params,i,j) : INF;
+					std::cout << "hairpin" <<std::endl;
+					std::cout << v_h << std::endl;
+		 			// info of best interior loop decomposition (if better than hairpin)
+		 			cand_pos_t best_l=0;
+		 			cand_pos_t best_k=0;
+		 			energy_t best_e;
+
+		 			energy_t v_iloop=INF;
+
+		 			cand_pos_t max_k = std::min(j-TURN-2,i+MAXLOOP+1);
+		 			if((p_table[i]<-1 && p_table[j] < -1) || p_table[i] == j) { 
+		 				for ( cand_pos_t k=i+1; k<=max_k; k++) {
+		 					cand_pos_t k_mod= k%(MAXLOOP+1);
+							
+		 					energy_t cank = (up_array[k-1]>=(k-i-1)-1);
+		 					cand_pos_t min_l=std::max(k+TURN+1, k+j-i- MAXLOOP-2);
+							
+		 					for (size_t l=j-1; l>=min_l; --l) {
+		 						assert(k-i+j-l-2<=MAXLOOP);
+		 						energy_t canl = ((up_array[j-1]>=(j-l-1)-1) | cank);
+		 						energy_t v_iloop_kl = INF & canl;
+								
+		 						v_iloop_kl = v_iloop_kl + V(k_mod,l) + E_IntLoop(k-i-1,j-l-1,ptype_closing,rtype[pair[S[k]][S[l]]],S1[i+1],S1[j-1],S1[k-1],S1[l+1],const_cast<paramT *>(params));
+							}
+						}
+					}
+		 			//const energy_t v_split = E_MbLoop(dmli1,dmli2,S,params,i,j,p_table);
+
+		 			//v = std::min(v_h,std::min(v_iloop,v_split));
+
+					v = std::min(v_h,v_iloop);
+					V(i_mod,j) = v;
+			} else {
+				V(i_mod,j) = INF;
+			} // end if (i,j form a canonical base pair)
+
+				
+		 	} // end loop j
+		} //end loop i
+		return V(1,n-1);
+	}
+
+
+/**
+ * @brief Fills the restriction arrays from CParty
+ * p_table will contain the index of each base pair
+ * X or x tells the program the base cannot pair and . sets it as unpaired but can pair
+ * Pseudoknots (denoted by [ ], < >, or { } ) are filled the same way as ( )
+ * That is, a structure like this (<)> is not possible.
+ * @param structure Input structure
+ * @param p_table Restricted array
+ * @param last_j_array Restricted Array
+ * @param in_pair_array Restricted Array
+ */
+void detect_restricted_pairs(auto const &structure, cand_pos_t *p_table, cand_pos_t *last_j_array, cand_pos_t *in_pair_array){
+	cand_pos_t i, j, count = 0, length = structure.length(),last_j=length;
+	std::vector<cand_pos_t>  pairs;
+	pairs.push_back(length);
+
+	for (i=length; i >=1; --i){
+		if ((structure[i-1] == 'x') || (structure[i-1] == 'X'))
+			p_table[i] = -1;
+		else if (structure[i-1] == '.')
+			p_table[i] = -2;
+		if (structure[i-1] == ')' || structure[i-1] == ']' || structure[i-1] == '}' || structure[i-1] == '>'){
+			pairs.push_back(i);
+			count++;
+		}
+		last_j_array[i] = pairs[pairs.size()-1];
+		in_pair_array[i] = count;
+		if (structure[i-1] == '(' || structure[i-1] == '[' || structure[i-1] == '{' || structure[i-1] == '<'){
+			j = pairs[pairs.size()-1];
+			pairs.erase(pairs.end()-1);
+			p_table[i] = j;
+			p_table[j] = i;
+			count--;
+		}
+	}
+	pairs.pop_back();
+	if (pairs.size() != 0)
+	{
+		fprintf (stderr, "The given structure is not valid: more left parentheses than right parentheses: \n");
+		exit (1);
+	}
+}
+
+/**
+* @brief Simple driver for CParty
+*
+* Reads sequence from command line or stdin and calls folding methods.
+*/
+int
+main(int argc,char **argv) {
+
+	args_info args_info;
+
+	// get options (call gengetopt command line parser)
+	if (cmdline_parser (argc, argv, &args_info) != 0) {
+	exit(1);
+	}
+
+	std::string seq;
+	if (args_info.inputs_num>0) {
+	seq=args_info.inputs[0];
+	} else {
+	std::getline(std::cin,seq);
+	}
+	cand_pos_t n = seq.length();
+	std::string restricted;
+    args_info.input_structure_given ? restricted = input_structure : restricted = std::string (n,'.');
+	if(restricted != "" && restricted.length() != n ){
+		std::cout << "input sequence and structure are not the same size" << std::endl;
+		exit(0);
+	}
+	std::string file= "";
+	args_info.paramFile_given ? file = parameter_file : file = "";
+	if(file!=""){
+		vrna_params_load(file.c_str(), VRNA_PARAMETER_FORMAT_DEFAULT);
+	}
+	
+
+	bool verbose;
+	verbose = args_info.verbose_given;
+
+
+	CParty cparty(seq,!args_info.noGC_given,restricted);
+
+	// Make replicate mx array in linear space
+	cand_pos_t last_j_array[n+1] = {0};
+	cand_pos_t in_pair_array[n+1] = {0};
+	cand_pos_t p_table[n+1] = {0};
+	cand_pos_t up_array[n+1] = {0};
+	
+	cmdline_parser_free(&args_info);
+
+	std::cout << seq << std::endl;
+	
+	detect_restricted_pairs(restricted,p_table,last_j_array,in_pair_array);
+	
+	energy_t partitionEnergy = fold(cparty.seq_,cparty.V_,cparty.S_,cparty.S1_,cparty.params_,cparty.n_,cparty.garbage_collect_, p_table,last_j_array,in_pair_array,up_array);		
+	std::cout << "partition" <<std::endl;
+	std::cout << partitionEnergy <<std::endl;
+
+	if (verbose) {
+		std::cout << "verbose" <<std::endl;
+	}
+
+	return 0;
+}
